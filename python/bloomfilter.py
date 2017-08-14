cb13907261b8689b3d950bf41ef7cfb0a7f9b91f
franck <franck.rupin@gmail.com>
2017-08-14 14:37:44 -0400

    Add a BloomFilter class
    - Add constructor
    - Add hash method
    - Add insert method

--- python/bloomfilter.py
+++ python/bloomfilter.py
@@ -48,20 +48,63 @@ def search(**kwargs):
 		return True
 
 
+class BloomFilter(object):
+	def __init__(self, bfSize=4096):
+		self.bfSize = bfSize
+		self.bitarray = bitarray(self.bfSize, endian='little')
+		self.bitarray.setall(False)
+
+	def hash(self, string):
+		return [int(hashlib.sha1(str(string).encode('utf-8')).hexdigest(),16) % self.bfSize,
+				 int(hashlib.sha256(str(string).encode('utf-8')).hexdigest(),16) % self.bfSize,
+				 int(hashlib.sha384(str(string).encode('utf-8')).hexdigest(),16) % self.bfSize,
+				 int(hashlib.sha512(str(string).encode('utf-8')).hexdigest(),16) % self.bfSize]
+
+
+	def insert(self, string):
+		keys = self.hash(string)
+		for k in keys:
+			self.bitarray[k] = True
+
+	def search(self, string):
+		keys = self.hash(string)
+		z = [ 1 if self.bitarray[k] == 1 else 0 for k in keys]
+		print(z)
+		if 0 in z:
+			return False
+		else:
+			return True
+
 
 if __name__=='__main__':
+	"""
 	m = 32
 	bf = [0] * m
-	
+	"""
+
 	"""
 	Simple hash functions for demonstration purpose only.
 	"""
+	"""
 	hashfuncs = (lambda x:(((x**2) + (x**3)) % m),
-	             lambda x:((((x**2) + (x**3))*2) % m),
-		     lambda x:((((x**2) + (x**3))*3) % m))
+				 lambda x:((((x**2) + (x**3))*2) % m),
+				 lambda x:((((x**2) + (x**3))*3) % m))
 
 	insert(f=hashfuncs, bf=bf,values=[2013,2010, 2007, 2004, 2001, 1998])
 	print(bf)
 	if search(f=hashfuncs, bf=bf,value=2013) is True:
-		print("value is present")
+			print("value is present")
+	"""
+	bf = BloomFilter()
+	bf.insert('tryeyuuuuueueire')
+
+	if bf.search('tryeyuuuuueueire'):
+		print( "Is present")
+	else:
+		print( "Is NOT  present")
+	if bf.search('DDDFFFdfdfdruueueire'):
+		print( "Is present")
+	else:
+		print( "Is NOT  present")
+	#print(bf.bitarray)
 
